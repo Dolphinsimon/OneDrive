cf8f836566987e20861703802bd5dff1d13c304b
Dolphinsimon <Dolphinsimon@live.com>
2015-07-20 16:43:21 +0800

    Fix some bugs to avoid exceptions when Directory.GetFiles() are called.

--- OneDriveAssistant.Console/OneDriveAssistantConsole.cs
+++ OneDriveAssistant.Console/OneDriveAssistantConsole.cs
@@ -12,7 +12,7 @@ namespace OneDriveAssistant
             {
                 WriteLine("Please enter the directory of Encoding_error.txt");
                 var path = ReadLine();
-                if (path == null)
+                if (path == null||!Directory.Exists(path))
                 {
                     WriteLine("Wrong input string,please enter the directory of Encoding_error.txt");
                 }
@@ -39,29 +39,37 @@ namespace OneDriveAssistant
                 }
             }
 
-            while (args.Length>0)
+            while (args.Length > 0)
             {
-                if (args.Length>1) WriteLine("Wrong args,please try again!");
-                var files = Directory.GetFiles(args[0], "Encoding Errors.txt");
-                if (files.Length < 1) continue;
-                if (files.All(file => file != "Encoding Errors.txt")) continue;
-                var myWorkFlow = new WorkFlow(args[0]);
-                var count = myWorkFlow.Rows.Length;
-                var myResult = new Result();
-                int once = 0;
-                foreach (var row in myWorkFlow.Rows)
+                if (args.Length > 1) WriteLine("Wrong args,please try again!");
+                if (args[0] == null || !Directory.Exists(args[0]))
                 {
-                    myResult = myWorkFlow.RestoreFileName(row);
-                    if (!myResult.Success) break;
-                    ++once;
-                    WriteLine("Process {0} %, total {1} files.", once / count * 100, once);
+                    WriteLine("Wrong input string,please enter the directory of Encoding_error.txt");
                 }
-                if (!myResult.Success)
-                    WriteLine("Process {0} failed, exception message is {1},please try again!", myResult.FileName, myResult.Error.Message);
-                if (myResult.Success)
-                    WriteLine("Congratulations! All files are generated successful, Enjoy your files!");
+                else
+                {
+                    var files = Directory.GetFiles(args[0], "Encoding Errors.txt");
+                    if (files.Length < 1) continue;
+                    if (files.All(file => file != "Encoding Errors.txt")) continue;
+                    var myWorkFlow = new WorkFlow(args[0]);
+                    var count = myWorkFlow.Rows.Length;
+                    var myResult = new Result();
+                    int once = 0;
+                    foreach (var row in myWorkFlow.Rows)
+                    {
+                        myResult = myWorkFlow.RestoreFileName(row);
+                        if (!myResult.Success) break;
+                        ++once;
+                        WriteLine("Process {0} %, total {1} files.", once/count*100, once);
+                    }
+                    if (!myResult.Success)
+                        WriteLine("Process {0} failed, exception message is {1},please try again!", myResult.FileName,
+                            myResult.Error.Message);
+                    if (myResult.Success)
+                        WriteLine("Congratulations! All files are generated successful, Enjoy your files!");
+                }
             }
-            
+
         }
     }
 }
